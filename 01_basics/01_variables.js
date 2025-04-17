fb6f7b18ac7ac983ad0758feb69dbec5462a7d6c
Akshad Khan <akshadkhan234@gmail.com>
2025-04-17 13:35:08 +0530

    learnt about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "akshad@google.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+let accountState;
+
+// accountId = 2 // not allowed since it is constant
+
+
+accountEmail = "ak@ak.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId); // 144553
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
